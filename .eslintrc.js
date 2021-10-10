ac31a5a09da6449d5fe763c26aa78eac2a654237
mrl5 <31549762+mrl5@users.noreply.github.com>
2021-10-10 11:49:46 +0200

    chore: setup unit tests runtime for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,5 @@ module.exports = {
         ecmaVersion: 2020,
         sourceType: 'module',
     },
-    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:mocha/recommended'],
 };
